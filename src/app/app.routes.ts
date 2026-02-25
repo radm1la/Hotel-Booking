7a8c90115d9fb5bf44a66eedd9bf604d5df11611
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-02-25 15:00:16 +0400

    navbar added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Hotels } from './hotels/hotels';
+import { Rooms } from './rooms/rooms';
 
 export const routes: Routes = [
-    {path:"",component:Home}
+    {path:"",component:Home},
+    {path:"hotels",component:Hotels},
+    {path:"rooms",component:Rooms}
 ];
