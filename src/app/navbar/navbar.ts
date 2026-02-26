7fa91a4584350876f3214af8f5c71cba9369de5f
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-02-26 22:04:17 +0400

    styles navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -6,7 +6,24 @@ import { RouterLink } from "@angular/router";
   imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
+  host: {
+    '[class.nav-hidden]': 'isHidden()'
+  }
 })
 export class Navbar {
+  isHidden = signal(false);
+  private lastScrollY = 0;
 
+  @HostListener('window:scroll',[])
+  onWindowScroll(){
+    const currentScroll = window.scrollY;
+
+    if(currentScroll > this.lastScrollY && currentScroll > 100){
+      this.isHidden.set(true);
+    }else{
+      this.isHidden.set(false);
+    }
+
+    this.lastScrollY = currentScroll;
+  }
 }
