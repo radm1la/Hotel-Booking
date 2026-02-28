dc752149f3c2fee2e3ff73532ced39802a1b1b5d
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-02-28 19:33:46 +0400

    working on not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Hotels } from './hotels/hotels';
 import { Rooms } from './rooms/rooms';
+import { NotFoundComponent } from './not-found-component/not-found-component';
 
 export const routes: Routes = [
     {path:"",component:Home},
     {path:"hotels",component:Hotels},
-    {path:"rooms/:hotelId",component:Rooms}
+    {path:"rooms/:hotelId",component:Rooms},
+    {path:"**",component:NotFoundComponent}
 ];
