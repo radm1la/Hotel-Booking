8248350853c035dfa68e43460f4c2f293d031863
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-02-28 17:42:16 +0400

    had errors on scrolling

--- src/app/hotels/hero-component-hotels/hero-component-hotels.ts
+++ src/app/hotels/hero-component-hotels/hero-component-hotels.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, signal } from '@angular/core';
+import { Component, computed, HostListener, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hero-component-hotels',
@@ -6,19 +6,19 @@ import { Component, HostListener, signal } from '@angular/core';
   templateUrl: './hero-component-hotels.html',
   styleUrl: './hero-component-hotels.css',
 })
-export class HeroComponentHotels {
+export class HeroComponentHotels implements OnInit {
   scrollY = signal(0);
+
+  ngOnInit(): void {
+    this.scrollY.set(window.scrollY);
+  }
+
+  heroTransform = computed(() => `translate3d(0, ${this.scrollY() * 0.4}px, 0)`);
+  textTransform = computed(() => `translate3d(0, ${this.scrollY() * 0.1}px, 0)`);
 
   @HostListener('window:scroll',[])
   onWindowScroll(){
     this.scrollY.set(window.scrollY);
   }
 
-  getHeroTransform(){
-    return `translate3d(0,${this.scrollY()*0.4}px,0)`;
-  }
-
-  getTextTransform(){
-    return `translate3d(0, ${this.scrollY() * 0.1}px, 0)`;
-  }
 }
