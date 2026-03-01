60e6f039714e02a194f101cbfdba8b9f2229d784
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-03-01 21:15:26 +0400

    finally structurised filter,next styling.

--- src/app/rooms/filtering-component/filtering-component.ts
+++ src/app/rooms/filtering-component/filtering-component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
 import { Helper } from '../../helper';
 
 @Component({
   selector: 'app-filtering-component',
-  imports: [ MatSliderModule,FormsModule ],
+  imports: [MatSliderModule, FormsModule],
   templateUrl: './filtering-component.html',
   styleUrl: './filtering-component.css',
 })
-export class FilteringComponent implements OnInit{
+export class FilteringComponent implements OnInit {
   roomTypes = signal<any>([]);
-  selectedRoomType = "0";
-  constructor(private service:Helper){}
+  selectedRoomType = '0';
+
+  checkInDate: string = '';
+  checkOutDate: string = '';
+  today = new Date().toISOString().split('T')[0];
+
+  maximumGuests: number = 1;
+
+  constructor(private service: Helper) {}
 
   ngOnInit(): void {
     this.fetchRoomTypes();
@@ -22,27 +29,53 @@ export class FilteringComponent implements OnInit{
   maxPrice = 800;
   priceLimit = 1000;
 
-  validateInputs(){
-    if(this.minPrice < 0){
+  validateInputs() {
+    if (this.minPrice < 0) {
       this.minPrice = 0;
     }
-    if(this.maxPrice > this.priceLimit){
+    if (this.maxPrice > this.priceLimit) {
       this.maxPrice = this.priceLimit;
     }
-    if(this.minPrice > this.maxPrice){
+    if (this.minPrice > this.maxPrice) {
       this.minPrice = this.maxPrice;
     }
   }
 
   //room types
-  fetchRoomTypes(){
+  fetchRoomTypes() {
     this.service.getRoomTypes().subscribe({
-      next:(data:any)=>{
+      next: (data: any) => {
         this.roomTypes.set(data);
       },
-      error:(bad)=>{
-        console.log("Error loading room types. ",bad);
+      error: (bad) => {
+        console.log('Error loading room types. ', bad);
+      },
+    });
+  }
+
+  //check in/out
+  validateDates() {
+    if (this.checkInDate && this.checkOutDate) {
+      if (this.checkInDate > this.checkOutDate) {
+        this.checkOutDate = this.checkInDate;
       }
-    })
+    }
+  }
+
+  formatDate(dateString: string): string {
+    if (!dateString) return '';
+    const date = new Date(dateString);
+    return date.toISOString();
+  }
+
+  //guests num
+
+  validateGuests() {
+    if (this.maximumGuests < 1) {
+      this.maximumGuests = 1;
+    }
+    if (this.maximumGuests > 6) {
+      this.maximumGuests = 6;
+    }
   }
 }
