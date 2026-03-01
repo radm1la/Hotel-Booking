50293344e23add084ed4e7b042e9e268d72706bd
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-03-01 18:11:32 +0400

    angular is nice for having their own slider that functions so well,nice

--- src/app/rooms/filtering-component/filtering-component.ts
+++ src/app/rooms/filtering-component/filtering-component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider';
 
 @Component({
   selector: 'app-filtering-component',
-  imports: [],
+  imports: [ MatSliderModule,FormsModule ],
   templateUrl: './filtering-component.html',
   styleUrl: './filtering-component.css',
 })
 export class FilteringComponent {
+  //price slider logic
+  minPrice = 100;
+  maxPrice = 800;
+  priceLimit = 1000;
 
+  changePrice(){
+
+  }
+
+  validateInputs(){
+    if(this.minPrice < 0){
+      this.minPrice = 0;
+    }
+    if(this.maxPrice > this.priceLimit){
+      this.maxPrice = this.priceLimit;
+    }
+    if(this.minPrice > this.maxPrice){
+      this.minPrice = this.maxPrice;
+    }
+  }
 }
