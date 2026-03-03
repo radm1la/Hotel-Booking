abb891d86753575c47460909db6a96012b057db2
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-03-03 14:56:09 +0400

    card loading animation

--- src/app/rooms/cards-component/cards-component.ts
+++ src/app/rooms/cards-component/cards-component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input } from '@angular/core';
+import { AfterViewInit, Component, computed, ElementRef, input, signal, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cards-component',
@@ -6,13 +6,27 @@ import { Component, effect, input } from '@angular/core';
   templateUrl: './cards-component.html',
   styleUrl: './cards-component.css',
 })
-export class CardsComponent {
-  roomsList = input<any[]>([]);
+export class CardsComponent implements AfterViewInit{
+  roomsList = input<any>([]);
   randomImgIndex = Math.floor(Math.random() * 6);
 
-  constructor(){
-    effect(() => {
-      console.log('Rooms List updated:', this.roomsList());
-    });
+  limit = signal(4);
+  @ViewChild('anchor') anchor!: ElementRef;
+
+  visibleRooms =  computed(()=>{
+    return this.roomsList().slice(0,this.limit());
+  })
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries)=>{
+      if(entries[0].isIntersecting && this.limit() < this.roomsList().length){
+        this.limit.update(n => n+2);
+      }
+    }, {threshold: 0.2}
+  )
+
+  observer.observe(this.anchor.nativeElement);
   }
 }
+
+
