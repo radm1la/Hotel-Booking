c573d1bafad599656171c82bc4de79220f8765df
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-03-08 16:58:53 +0400

    add component to room detail

---
+++ src/app/room-detail/book-room/book-room.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-room',
+  imports: [],
+  templateUrl: './book-room.html',
+  styleUrl: './book-room.css',
+})
+export class BookRoom {
+
+}
