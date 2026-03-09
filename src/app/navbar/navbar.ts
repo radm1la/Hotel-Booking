562c77e148d88321feeebfedf10a93e96b8b1602
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-03-10 00:10:13 +0400

    issues with saving cookies

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,9 +1,8 @@
 import { Component, HostListener, signal } from '@angular/core';
-import { NavigationEnd, Router, RouterLink } from "@angular/router";
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
 import { Helper } from '../helper';
 import { CookieService } from 'ngx-cookie-service';
-
 
 @Component({
   selector: 'app-navbar',
@@ -11,32 +10,38 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
   host: {
-    '[class.nav-hidden]': 'isHidden()'
-  }
+    '[class.nav-hidden]': 'isHidden()',
+  },
 })
 export class Navbar {
   isHidden = signal(false);
   private lastScrollY = 0;
   isLogged = false;
 
-  checkAuth(){
-    this.service.isLogged.subscribe((data)=>{
+  checkAuth() {
+    const token = this.cookie.get('user_token');
+    if (token) {
+      this.isLogged = true;
+      this.service.isLogged.next(true); 
+    }
+
+    this.service.isLogged.subscribe((data) => {
       this.isLogged = data;
-    })
+    });
   }
 
-  logOut(){
+  logOut() {
     this.service.isLogged.next(false);
-    this.cookie.delete("user_token");
+    this.cookie.delete('user_token');
   }
 
-  @HostListener('window:scroll',[])
-  onWindowScroll(){
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
     const currentScroll = window.scrollY;
 
-    if(currentScroll > this.lastScrollY && currentScroll > 100){
+    if (currentScroll > this.lastScrollY && currentScroll > 100) {
       this.isHidden.set(true);
-    }else{
+    } else {
       this.isHidden.set(false);
     }
 
@@ -45,13 +50,17 @@ export class Navbar {
 
   currentUrl: string = '';
 
-  constructor(private router: Router,private service:Helper,private cookie:CookieService) {
+  constructor(
+    private router: Router,
+    private service: Helper,
+    private cookie: CookieService,
+  ) {
     this.checkAuth();
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: any) => {
-      this.currentUrl = event.urlAfterRedirects;
-    });
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.currentUrl = event.urlAfterRedirects;
+      });
   }
 
   isActive(path: string): boolean {
@@ -60,6 +69,4 @@ export class Navbar {
     }
     return this.currentUrl.includes(path);
   }
-
-
 }
