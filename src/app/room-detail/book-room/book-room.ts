55232e942fe596a5f9fb2fca409cd1a351a80fa0
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-03-10 16:42:54 +0400

    working on booking

--- src/app/room-detail/book-room/book-room.ts
+++ src/app/room-detail/book-room/book-room.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, input, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, input, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Helper } from '../../helper';
 
@@ -8,23 +8,42 @@ import { Helper } from '../../helper';
   templateUrl: './book-room.html',
   styleUrl: './book-room.css',
 })
-export class BookRoom implements AfterViewInit {
+export class BookRoom implements OnInit{
   roomInfo = input<any>();
   today = new Date().toISOString().split('T')[0];
+  private cdr = inject(ChangeDetectorRef);
+  errorMessage: string = '';
+  islogged = false;
 
   totalPrice = signal(0);
 
   bookingForm = new FormGroup({
     checkInDate: new FormControl('', [Validators.required]),
     checkOutDate: new FormControl('', [Validators.required]),
-    customerName: new FormControl('', [Validators.required]),
-    customerPhone: new FormControl('', [Validators.required]),
+    customerName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(20),
+    ]),
+    customerPhone: new FormControl('+995', [
+      Validators.required,
+      Validators.pattern(/^\+995\d{9}$/),
+    ]),
   });
 
-  constructor(private service: Helper) {}
+  constructor(private service: Helper) {
+    this.bookingForm.valueChanges.subscribe(() => {
+      this.errorMessage = '';
+      this.cdr.detectChanges();
+    });
+  }
 
   ngOnInit() {
     this.bookingForm.valueChanges.subscribe(() => this.calculatePrice());
+    this.service.isLogged.subscribe((st)=>{
+      this.islogged = st;
+      this.cdr.detectChanges();
+    })
   }
 
   calculatePrice() {
@@ -40,25 +59,36 @@ export class BookRoom implements AfterViewInit {
   }
 
   submitBooking() {
+    this.errorMessage = '';
     if (this.bookingForm.valid) {
       const payload = {
         ...this.bookingForm.value,
         roomID: this.roomInfo().id,
         totalPrice: this.totalPrice(),
         isConfirmed: true,
-        customerId: localStorage.getItem('user_id'), 
+        customerId: localStorage.getItem('user_id'),
       };
 
       this.service.postBooking(payload).subscribe({
         next: (res: any) => alert('Booking Successful!'),
-        error: (err: any) => console.error(err),
+        error: (err: any) => {
+          if (err.status == 400) {
+            console.log(err);
+            
+            this.errorMessage = err.error.error;
+            this.cdr.detectChanges();
+          }
+        },
       });
     }
   }
 
-  ngAfterViewInit(): void {
-    if ((window as any).__weatherwidget_init) {
-      (window as any).__weatherwidget_init();
-    }
+  hasError(controlName: string, errorType: string): boolean {
+    const control = this.bookingForm.get(controlName);
+    return !!(
+      control?.invalid &&
+      (control?.dirty || control?.touched) &&
+      control?.hasError(errorType)
+    );
   }
 }
