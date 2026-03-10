86032bd3631ce64070c9f5b77a72658c1e39b98a
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-03-10 22:17:42 +0400

    DONE BOOKINGS.TIRED

--- src/app/my-booking/my-booking.ts
+++ src/app/my-booking/my-booking.ts
@@ -12,12 +12,17 @@ import { Router } from '@angular/router';
 })
 export class MyBooking {
   isLocalLoading = signal(true);
-  constructor(public service: Helper,private router:Router) {
+  isDeleting = signal<number[]>([]);
+  booking = signal<any>([]);
+  userInfo = signal<any>({});
+
+  constructor(
+    public service: Helper,
+    private router: Router,
+  ) {
     this.fetchBooking();
     this.fetchUser();
   }
-  booking = signal<any>([]);
-  userInfo = signal<any>({});
 
   fetchBooking() {
     this.isLocalLoading.set(true);
@@ -25,27 +30,27 @@ export class MyBooking {
 
     this.service.getBooking().subscribe((data: any) => {
       const myBookings = data.filter((i: any) => i.customerId === currentUserId);
-      
-      
+      console.log(myBookings);
+
       if (myBookings.length === 0) {
         this.booking.set([]);
         this.isLocalLoading.set(false);
         return;
       }
 
-      const roomReqs = myBookings.map((b:any)=>{
+      const roomReqs = myBookings.map((b: any) => {
         return this.service.getRoomById(b.roomID).pipe(
-          switchMap((roomDetails:any)=>
+          switchMap((roomDetails: any) =>
             this.service.getHotelById(roomDetails.hotelId).pipe(
-              map((hotelDetails:any)=>({
+              map((hotelDetails: any) => ({
                 ...b,
                 roomDetails,
-                hotelDetails
-              }))
-            )
-          )
-        )
-      })
+                hotelDetails,
+              })),
+            ),
+          ),
+        );
+      });
 
       forkJoin(roomReqs).subscribe({
         next: (data) => {
@@ -53,47 +58,49 @@ export class MyBooking {
           this.booking.set(data);
           this.isLocalLoading.set(false);
         },
-        error:(err)=>{
-          console.error("Error fetching room details", err);
+        error: (err) => {
+          console.error('Error fetching room details', err);
           this.isLocalLoading.set(false);
-        }
-      })
+        },
+      });
     });
   }
 
-  fetchUser(){
+  fetchUser() {
     this.service.getUser().subscribe({
-      next:(data)=>{
+      next: (data) => {
         this.userInfo.set(data);
       },
-      error:(err)=>{
-        console.log("Error loading user info",err);
-      }
-    })
-  }
-
-  deleteAcc(){
-    this.service.deleteAccount().subscribe({
-      next:(data)=>{
-        console.log(data);
-        this.router.navigate(["/"]);
+      error: (err) => {
+        console.log('Error loading user info', err);
       },
-      error:(err)=>{
-        console.log("Erro deleting user account",err);
-      }
-    })
+    });
   }
 
-  cancel(id:number){
-    this.service.cancelBooking(id).subscribe({
-      next:(data)=>{
+  deleteAcc() {
+    this.service.deleteAccount().subscribe({
+      next: (data) => {
         console.log(data);
-        this.fetchBooking();
-      },error:(err)=>{
-        console.log("Error calceling booking",err);
-        
-      }
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.log('Erro deleting user account', err);
+      },
+    });
+  }
 
-    })
+  cancel(id: number) {
+    this.isDeleting.update((ids) => [...ids, id]);
+    this.service.cancelBooking(id).subscribe({
+      next: (data) => {
+        this.booking.update((current) => current.filter((b: any) => b.id !== id));
+        this.isDeleting.update((ids) => ids.filter((itemId) => itemId !== id));
+      },
+      error: (err) => {
+        console.log('Error calceling booking', err);
+        this.isDeleting.update((ids) => ids.filter((itemId) => itemId !== id));
+        alert('Could not cancel this reservation. Please try again.');
+      },
+    });
   }
 }
